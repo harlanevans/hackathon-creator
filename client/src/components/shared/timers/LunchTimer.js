6968f5f8c6bc2c9c0ade6201ca790864151e572b
Gabriella Algecera <gabriellaalgecera@Brians-MacBook-Pro.local>
2020-01-23 18:14:02 -0700

    figured out how to set timer

---
+++ client/src/components/shared/timers/LunchTimer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class LunchTimer extends Component {
+    render() {
+        return(
+            <>
+
+            </>
+        )
+    }
+}
+
+export default LunchTimer;
