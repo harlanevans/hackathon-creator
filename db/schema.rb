26c86b8b0ab6401959984297ceb2e433bae96a70
Gabriella Algecera <gabriellaalgecera@Brians-MacBook-Pro.local>
2020-01-25 16:32:14 -0700

    add active column to timers table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_25_232211) do
+ActiveRecord::Schema.define(version: 2020_01_25_233136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,6 +84,7 @@ ActiveRecord::Schema.define(version: 2020_01_25_232211) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "types"
+    t.boolean "active"
   end
 
   create_table "users", force: :cascade do |t|
