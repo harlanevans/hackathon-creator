89fe02aa4f86f70b6816c489a8e1a6cb71de336d
Gabriella Algecera <gabriellaalgecera@Brians-MacBook-Pro.local>
2020-01-25 15:41:08 -0700

    fix event edit bug

--- client/src/components/shared/courses/CourseShow.js
+++ client/src/components/shared/courses/CourseShow.js
@@ -9,7 +9,7 @@ class CourseShow extends Component {
       <>
         <h1>{this.props.location.state.name}</h1>
         <Students course_id={this.props.location.state.id} />
-        <Events course_id={this.props.location.state.id} />
+        <Events course_id={this.props.location.state.id} updateEvent={this.updateEvent}/>
       </>
     );
   }
--- client/src/components/shared/students/StudentForm.js
+++ client/src/components/shared/students/StudentForm.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import { Form, Button } from 'semantic-ui-react';
 
+const lvl_choices = [ { key: "1", text: "1", value: 1},
+{ key: "2", text: "2", value: 2},
+{ key: "3", text: "3", value: 3},
+{ key: "4", text: "4", value: 4},
+{ key: "5", text: "5", value: 5} ]
 
 class StudentForm extends Component {
 
@@ -73,11 +78,5 @@ class StudentForm extends Component {
     )
   }
 }
-
-const lvl_choices = [ { key: "1", text: "1", value: 1},
-{ key: "2", text: "2", value: 2},
-{ key: "3", text: "3", value: 3},
-{ key: "4", text: "4", value: 4},
-{ key: "5", text: "5", value: 5} ]
 
 export default StudentForm;
