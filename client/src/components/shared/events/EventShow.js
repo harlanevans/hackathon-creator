bf74e9b2e09732135631d241dfe7a332a116e613
Somchay <Somchayk@yahoo.com>
2020-01-25 16:47:30 -0700

    rubric

--- client/src/components/shared/events/EventShow.js
+++ client/src/components/shared/events/EventShow.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Groups from '../groups/Groups'
 import Submissions from '../submissions/Submissions';
+import EventForm from './EventForm';
+import Rubric from '../Rubric';
 
 class EventShow extends React.Component {
   render() {
-    const { name, id, course_id } = this.props.location.state;
+    const { name, id, course_id, rubric } = this.props.location.state;
 
     return(
       <div>
@@ -12,9 +14,11 @@ class EventShow extends React.Component {
        <h1> {name}</h1>
        <Groups course_id={course_id} event_id={id}/>
        <Submissions course_id={course_id} event_id={id} />
+       <Rubric></Rubric>
       </div>
     )
   }
 }
-
+//"https://drive.google.com/file/d/1h8vrAUdPcQ0Op50aGhQU5Htbueaot4XG/preview"
+//"https://drive.google.com/file/d/1h8vrAUdPcQ0Op50aGhQU5Htbueaot4XG/view"
 export default EventShow;
