0b2575947700c17c3641c4ce730590e5830d3437
Gabriella Algecera <gabriellaalgecera@Brians-MBP.domain>
2020-01-26 15:25:51 -0700

    create timer form

---
+++ client/src/components/shared/timers/TimerForm.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import { Form, Button } from 'semantic-ui-react';
+
+const active_dropdown = [ { key: "yes", text: "yes", value: true},
+{ key: "no", text: "no", value: false} ]
+
+const types_dropdown = [ { key: "event", text: "event", value: "event"},
+{ key: "lunch", text: "lunch", value: "lunch"} ]
+
+class TimerForm extends Component {
+
+  state = {
+    name: '', 
+    start_time: '', 
+    end_time: '',
+    types: '', 
+    active: null
+  } 
+
+  componentDidMount() {
+    const { id, name, start_time, end_time, types, active } = this.props
+    if (id) {
+      this.setState({
+        id,
+        name,
+        start_time,
+        end_time,
+        complete,
+        types,
+        active
+      })
+    }
+  }
+
+  handleChange = (e, { name, value }) => this.setState({ [name]: value })
+
+  handleSumbit = (e) => {
+    e.preventDefault()
+    if (this.props.id) {
+      this.props.updateTimer(this.props.course_id, this.props.id, this.state)
+      this.props.toggleEdit()
+      this.setState({ name: '', start_time: '', end_time: '', types: '', active: null })
+    } else {
+      this.props.addTimer(this.state)
+      this.props.toggleAdd()
+      this.setState({ name: '', start_time: '', end_time: '', types: '', active: null })
+    }
+    }
+
+  render() {
+    const { id, name, start_time, end_time, types, active } = this.state
+    return(
+      <Form onSubmit={ this.handleSumbit }>     
+        <Form.Group>
+          <Form.Input 
+            name='name'
+            value={name}
+            onChange={this.handleChange}
+            label='Timer Name'
+            required
+            />
+          <Form.Input
+            type={<input type='time'/>}
+            name='start_time'
+            value={start_time}
+            onChange={this.handleChange}
+          />
+          <Form.Input
+            type={<input type='time'/>}
+            name='end_time'
+            value={end_time}
+            onChange={this.handleChange}
+            />
+          <Form.Select
+            name='type'
+            value={types}
+            options={types_dropdown}
+            onChange={this.handleChange}
+            label='type'
+            required
+            />
+         <Form.Select
+            name='active'
+            value={active}
+            options={active_dropdown}
+            onChange={this.handleChange}
+            label='Active'
+            required
+            />
+        </Form.Group>
+        <Form.Group>
+          <Form.Button>{this.props.id ? "Update Timer" : "Create Timer" }</Form.Button>
+          <Button onClick={this.props.id ? this.props.toggleEdit : this.props.toggleAdd}>Cancel</Button>
+        </Form.Group>
+      </Form>
+    )
+  }
+}
+
+export default TimerForm;
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,20 @@
-1.times do course = Course.create(
-    name: 'Winter Part-Time 2019'
-)
-1.times do event = Event.create(
-    name: 'Hackathon I 2019',
-    rubric: '',
-    course_id = course.id
-)
+# 1.times do course = Course.create(
+#     name: 'Winter Part-Time 2019'
+# )
+# 1.times do event = Event.create(
+#     name: 'Hackathon I 2019',
+#     rubric: '',
+#     course_id = course.id
+# )
 
-
+1.times do Timer.create(
+    name: "Hackathon I 2020",
+    start_time: "09:00:00",
+    end_time: "17:00:00",
+    complete: false, 
+    types: "event",
+    active: true
+)
 end
 
 puts 'Data seeded'
