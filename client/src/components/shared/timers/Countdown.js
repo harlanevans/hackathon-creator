697abbefe319d3c3a30ba10aaaf33f3426a460a0
Aaron Cookson <cookson.aaron@gmail.com>
2020-01-27 00:56:58 -0700

    added a completed feature to timers

--- client/src/components/shared/timers/Countdown.js
+++ client/src/components/shared/timers/Countdown.js
@@ -8,18 +8,23 @@ class Countdown extends Component {
     hours: undefined,
     minutes: undefined,
     seconds: undefined,
+    completed: undefined
   }
 
   componentDidMount() {
     this.interval = setInterval(() => {
-        const { timeTillDate, timeFormat } = this.props;
+        const { timeTillDate } = this.props;
         const then = moment(timeTillDate, "HH:mm");
         const now = moment();
         const countdown = moment.utc(then - now);
         const hours = countdown.format('H');
         const minutes = countdown.format('m');
         const seconds = countdown.format('s');
-        this.setState({ hours, minutes, seconds });
+        if (countdown._i < 0 ) {
+          this.setState({completed: true})
+        } else {
+          this.setState({ hours, minutes, seconds, completed: false, countdown });
+        }
     }, 1000);
   }
 
@@ -29,25 +34,32 @@ class Countdown extends Component {
     }
   }
 
+
+
   render() {
-    const { hours, minutes, seconds } = this.state;
+    const { hours, minutes, seconds, completed } = this.state;
     return(
       <div>
         {this.props.types === 'lunch' ? <h1>Lunch Timer</h1> : <h1>Hackathon Countdown</h1>}
-        <div className="countdown-wrapper">
-            <div className="countdown-item">
-                {hours}
-                <span>hours</span>
-            </div>
-            <div className="countdown-item">
-                {minutes}
-                <span>minutes</span>
-            </div>
-            <div className="countdown-item">
-                {seconds}
-                <span>seconds</span>
-            </div>
-        </div>
+        {
+          completed ?
+          this.props.types === 'lunch' ? <h1>START CODING!</h1> : <h1>TIME'S UP!</h1>
+          :
+          <div className="countdown-wrapper">
+              <div className="countdown-item">
+                  {hours}
+                  <span>hours</span>
+              </div>
+              <div className="countdown-item">
+                  {minutes}
+                  <span>minutes</span>
+              </div>
+              <div className="countdown-item">
+                  {seconds}
+                  <span>seconds</span>
+              </div>
+          </div>
+        }
         <h4>End Time: {moment(this.props.timeTillDate, "HH:mm").format('LT')}</h4>
       </div>
     )
