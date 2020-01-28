e927b1c53b71445ada0364c5d1032303f02e5f3e
Somchay <Somchayk@yahoo.com>
2020-01-27 19:08:33 -0700

    removing errors

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,5 @@
 User.delete_all
 Timer.delete_all
-<<<<<<< HEAD
 
 User.create(
     email: "admin@test.com",
@@ -14,26 +13,10 @@ Timer.create(
     active: false
 )
 
-=======
-User.create(
-    email: "admin@test.com",
-    password: "password11"
-)
-Timer.create(
-    name: 'Lunch',
-    end_time: '12:00:00',
-    types: 'lunch',
-    active: false
-)
->>>>>>> removing errors
 Timer.create(
     name: 'Event',
     end_time: '17:00:00',
     types: 'event',
     active: false
 )
-<<<<<<< HEAD
-
-=======
->>>>>>> removing errors
 puts 'Data seeded'
