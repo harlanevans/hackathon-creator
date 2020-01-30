7d244819c9f13ef99de2a18f099addebd3d81834
Dani DeSalvio <danidvisuals@gmail.com>
2020-01-30 10:54:18 -0700

    final style touches

--- client/src/components/shared/courses/CourseShow.js
+++ client/src/components/shared/courses/CourseShow.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 import Students from '../students/Students';
 import Events from '../events/Events';
+import { Container } from 'semantic-ui-react';
+import { Sections } from '../../styled-components/Shared';
+
 
 class CourseShow extends Component {
 
   render(){
     return (
       <>
-        <h1>{this.props.location.state.name}</h1>
-        <Students course_id={this.props.location.state.id} />
+        <Container fluid>
+          <div className="image-header admin">
+            <h1>{this.props.location.state.name}</h1>
+              <div className="img-overlay"></div>
+          </div>
+        </Container>
+        
+        <Sections><Students course_id={this.props.location.state.id} /></Sections>
         <Events course_id={this.props.location.state.id} updateEvent={this.updateEvent}/>
       </>
     );
--- client/src/components/shared/events/EventShow.js
+++ client/src/components/shared/events/EventShow.js
@@ -3,6 +3,7 @@ import Groups from '../groups/Groups'
 import Submissions from '../submissions/Submissions';
 import Rubric from '../Rubric';
 import { Sections } from '../../styled-components/Shared';
+import { Container } from 'semantic-ui-react';
 
 
 class EventShow extends React.Component {
@@ -11,12 +12,22 @@ class EventShow extends React.Component {
 
     return(
       <div className="eventshow-wrapper">
-       <Sections>
-        <h1> {name}</h1>
-      </Sections> 
-       <Groups course_id={course_id} event_id={id}/>
-       <Rubric rubric={rubric}/>
-       <Submissions course_id={course_id} event_id={id} />
+        <Container fluid>
+            <div className="image-header admin">
+            <h1> {name}</h1>
+                <div className="img-overlay"></div>
+            </div>
+        </Container>
+        
+        <Sections>
+          <Groups course_id={course_id} event_id={id}/>
+        </Sections> 
+
+        <Rubric rubric={rubric}/>
+
+        <Sections>
+          <Submissions course_id={course_id} event_id={id} />
+        </Sections>
       </div>
     )
   }
